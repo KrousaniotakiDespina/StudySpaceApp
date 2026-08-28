a5e3745807621a35afa0997e94fb70567218fcf3
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-08-28 21:19:15 +0300

    feat: add todotask model

---
+++ StudySpaceApp/Models/TodoTask.cs
@@ -0,0 +1,13 @@
+﻿namespace StudySpaceApp.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public bool IsCompleted { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
