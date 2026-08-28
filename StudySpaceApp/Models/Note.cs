6f88fe9c2e474555dc18085260d76fe74dcaa968
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-08-28 21:18:38 +0300

    feat: add note model

---
+++ StudySpaceApp/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace StudySpaceApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
