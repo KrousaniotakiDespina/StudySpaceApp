402ace8a634e2b7c9bde728fda73e9f89ec61f58
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-08-28 21:18:24 +0300

    feat: add user model

---
+++ StudySpaceApp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace StudySpaceApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; } = null!;
+
+        public string Lastname { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Theme { get; set; } = "blue";
+    }
+}
