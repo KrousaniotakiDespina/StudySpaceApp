37b1288d5091df29408d28c7490344317c54a5f6
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-08-30 18:21:09 +0300

    chore: added service layer

---
+++ StudySpaceApp/Service/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using StudySpaceApp.DTO;
+
+namespace StudySpaceApp.Service
+{
+    public interface IUserService
+    {
+        UserReadOnlyDTO? Login(UserLoginDTO loginDTO);
+    }
+}
---
+++ StudySpaceApp/Service/UserServiceImpl.cs
@@ -0,0 +1,40 @@
+﻿using StudySpaceApp.DAO;
+using StudySpaceApp.DTO;
+using StudySpaceApp.Models;
+
+namespace StudySpaceApp.Service
+{
+    public class UserServiceImpl : IUserService
+    {
+        private readonly IUserDAO _userDAO;
+
+        public UserServiceImpl(IUserDAO userDAO)
+        {
+            _userDAO = userDAO;
+        }
+
+        public UserReadOnlyDTO? Login(UserLoginDTO loginDTO)
+        {
+            User? user = _userDAO.GetUserByEmail(loginDTO.Email);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (user.Password != loginDTO.Password)
+            {
+                return null;
+            }
+
+            return new UserReadOnlyDTO
+            {
+                Id = user.Id,
+                Firstname = user.Firstname,
+                Lastname = user.Lastname,
+                Email = user.Email,
+                Theme = user.Theme
+            };
+        }
+    }
+}
