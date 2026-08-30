485f882905a062edcf9bde76e6b657813a4d5ecf
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-08-30 18:25:28 +0300

    feat: connect login page to authentication service

--- StudySpaceApp/Pages/Login.cshtml.cs
+++ StudySpaceApp/Pages/Login.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudySpaceApp.DTO;
+using StudySpaceApp.Service;
 
 namespace StudySpaceApp.Pages
 {
     public class LoginModel : PageModel
     {
+        private readonly IUserService _userService;
+        public LoginModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [BindProperty]
+        public UserLoginDTO LoginDTO { get; set; } = new();
+
+        public string? ErrorMessage { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            var user = _userService.Login(LoginDTO);
+
+            if (user == null)
+            {
+                ErrorMessage = "Invalid email or password.";
+
+                return Page();
+            }
+
+            return RedirectToPage("/Dashboard");
         }
     }
 }
