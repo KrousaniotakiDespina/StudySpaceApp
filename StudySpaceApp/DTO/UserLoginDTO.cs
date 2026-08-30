b34846a30b2cabd138aa23009490ad3866c1bf18
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-08-30 16:17:36 +0300

    feat: add dto for user

---
+++ StudySpaceApp/DTO/UserLoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StudySpaceApp.DTO
+{
+    public class UserLoginDTO
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ StudySpaceApp/DTO/UserReadOnlyDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace StudySpaceApp.DTO
+{
+    public class UserReadOnlyDTO
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; } = null!;
+
+        public string Lastname { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Theme { get; set; } = null!;
+    }
+}
