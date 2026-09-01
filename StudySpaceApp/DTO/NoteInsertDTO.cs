0db4519328cab9dcbe90bf2dc2cc699c839c4990
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-09-01 11:30:04 +0300

    feat: add note DTO

---
+++ StudySpaceApp/DTO/NoteInsertDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StudySpaceApp.DTO
+{
+    public class NoteInsertDTO
+    {
+        public string Content { get; set; } = null!;
+
+        public int UserId { get; set; }
+    }
+}
---
+++ StudySpaceApp/DTO/NoteReadOnlyDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace StudySpaceApp.DTO
+{
+    public class NoteReadOnlyDTO
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
